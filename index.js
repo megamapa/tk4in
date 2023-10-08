8a3a83d5719661ea45c3dc548cdc097b62d12707
Paulo <megamapa@gmail.com>
2023-10-08 01:20:54 -0300

    Small changes

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const express = require('express');
 const app = express();
 
 // Certificate
-//const privateKey = fs.readFileSync('/etc/letsencrypt/live/tk4.in/privkey.pem', 'utf8');
-//const certificate = fs.readFileSync('/etc/letsencrypt/live/tk4.in/cert.pem', 'utf8');
-//const ca = fs.readFileSync('/etc/letsencrypt/live/tk4.in/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tk4.in/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tk4.in/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/tk4.in/fullchain.pem', 'utf8');
 
-//const credentials = {
-//	key: privateKey,
-//	cert: certificate,
-//	ca: ca
-//};
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.use((req, res) => {
 	res.send('Hello there !');
@@ -23,12 +23,12 @@ app.use((req, res) => {
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-//const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-//httpsServer.listen(443, () => {
-//	console.log('HTTPS Server running on port 443');
-//});
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
