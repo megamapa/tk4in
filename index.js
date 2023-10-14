466fdb0754f607ed9179ca73ced42148d175457b
Paulo <megamapa@gmail.com>
2023-10-14 19:10:20 -0300

    Small changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Version = '2.0.0';
 var starttime;
 
 /****************************************************************************************************/
-/* Funções úteis   																					*/
+/* Funções úteis																					*/
 /****************************************************************************************************/
 async function GetDate() {
 	let offset = new Date(new Date().getTime()).getTimezoneOffset();
@@ -23,7 +23,7 @@ async function RandomNum(min, max) {
 // Gera uma USID - Unique Session ID
 async function GetUSID() {
 	res1 = await RandomNum(111,999);
-    res2 = await RandomNum(20199,99199);
+	res2 = await RandomNum(20199,99199);
 	res3 = await RandomNum(10,99);
 	res4 = await RandomNum(10,99);
 	res5 = await RandomNum(10199,99999);
@@ -68,43 +68,46 @@ setInterval(function() {
 },60000);
 
 /****************************************************************************************************/
-/* Inicializa o http2																		        */
+/* Inicializa o http2																				*/
 /****************************************************************************************************/
-const fs = require('node:fs');
 // Le o certificado
+const fs = require('node:fs');
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/tk4.in/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/tk4.in/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/tk4.in/fullchain.pem', 'utf8');
+
 // Cria o servidor
 const http2 = require('node:http2');
 const server = http2.createSecureServer({
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-  allowHTTP1 : true
-});
+	key: privateKey,
+	cert: certificate,
+	ca: ca,
+	allowHTTP1 : true
+}, onRequest);
 
 server.listen(443, () => {
 	GetDate().then(dte =>{console.log('\033[36m'+dte+': \033[32mHTTPS Server rodando na porta 443.\033[0;0m');});
 });
 
+server.on('error', (err) => GetDate().then(dte =>{console.log('\033[31m'+dte+': \033[32mErro no HTTP2.\033[0;0m');}));
+
 /****************************************************************************************************/
-/* Rotinas do http2																			        */
+/* Rotinas do http2																					*/
 /****************************************************************************************************/
 async function GetSession(headers) {
 	// Inicializa a sessao
 	let	session = {
 		startTime: await GetDate(),
+		cookies :{},
 		remoteAddress: {IPv4: '', IPv6: ''},
 		login : '*',
 		map : 'MB',
 		mapset : ['MB'],
 		lang : 'en-US',
-		cookies :{},
 	};
 	// Le os cookies
 	let str = headers['cookie'];
-	
+
 	//const lang = headers['accept-language'];
 
 	// Se nao tiver um cookie cria um novo
@@ -121,15 +124,64 @@ async function GetSession(headers) {
 	session.USID = USID;
 	await hub.hset('ses:'+USID, session);
 	// Retorna uma nova sessão
-	//						console.log(JSON.stringify(session, null, 2));
+	// console.log(JSON.stringify(session, null, 2));
 	return(session);
 }
 /****************************************************************************************************/
-/* Mensagens do http2																		        */
+/* Mensagens do http2																				*/
 /****************************************************************************************************/
-server.on('error', (err) => console.log('\033[36m'+dte+': \033[32mErro no HTTP2.\033[0;0m'));
+function onRequest(req, res) {
+	// Verifica se a conexão e HTTP/1 ou HTTP/2
+	const { socket: { alpnProtocol } } = req.httpVersion === '2.0' ? req.stream.session : req;
+	// Responde
+	console.log(req);
+	const path = "/";
+	switch(path) {
+			case '/': {
+				res.writeHead(200, { 
+					'access-control-allow-methods': 'GET,POST',
+					'access-control-allow-origin': "'"+process.env.WWWBase+"'",
+					'cache-control': 'no-cache',
+					//'content-encoding': 'gzip',
+					'content-security-policy': "default-src 'self'; base-uri 'self'; script-src 'report-sample' 'nonce-"+nonce+"' cdn.jsdelivr.net/npm/ "+process.env.CDNBase+"; style-src 'self' 'report-sample' cdn.jsdelivr.net/npm/ "+process.env.CDNBase+"; object-src 'none'; frame-src 'self'; frame-ancestors 'none'; img-src 'self' "+process.env.CDNBase+"; font-src cdnjs.cloudflare.com/ajax/libs/font-awesome/; connect-src 'self' *.mapbox.com/; form-action 'self'; media-src 'self'; worker-src 'self'",
+					'content-type': 'text/html; charset=UTF-8',
+					'date': new Date().toUTCString(),
+					'permissions-policy': 'geolocation=(self "'+process.env.WWWBase+'")',
+					'referrer-policy': "no-referrer-when-downgrade",
+					'set-cookie': 'tk_v='+session.USID+'; Domain='+process.env.CKEBase+'; Path=/; Secure; HttpOnly', [http2.sensitiveHeaders]: ['set-cookie'],
+					'set-cookie': 'cross-site-cookie=name; SameSite=None; Secure; HttpOnly',
+					'strict-transport-security':'max-age=31536000; includeSubDomains; preload',
+					'vary': 'Accept-Encoding',
+					'x-content-type-options': 'nosniff',
+					'x-frame-options': 'DENY',
+					'x-permitted-cross-domain-policies': 'none',
+					'x-xss-protection': '1; mode=block' });
+				// Header
+				res.write("<!DOCTYPE html><html itemscope itemtype='http://schema.org/WebSite'; lang="+session.lang+"><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta charset=utf-8><title itemprop=name>"+process.env.IndexTit+"</title><link rel=dns-prefetch href="+process.env.CDNBase+"><link rel=canonical href="+process.env.WWWBase+" itemprop=url><link rel=icon href='"+process.env.CDNBase+"img/logo.png' itemprop=image><link rel=preload href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/webfonts/fa-regular-400.woff2' as=font type='font/woff2' crossorigin=anonymous><link rel=preload href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/webfonts/fa-solid-900.woff2' as=font type='font/woff2' crossorigin=anonymous><meta name=description content='"+process.env.IndexDes+"' itemprop=description><meta name=keywords content='"+process.env.IndexKey+"'><meta name=apple-mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-status-bar-style content=black-translucent><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' rel=stylesheet integrity='sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9' crossorigin=anonymous><link href='"+process.env.CDNBase+"css/style.css' rel=stylesheet crossorigin=anonymous></head><body>");
+				// Block
+				res.write("<div class=loader-wrap id=loader-wrap><div class=blocks><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div></div></div>");
+				// Scripts
+				res.write("</body><script async src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm' crossorigin=anonymous></script><script nonce="+nonce+">const es=document.getElementsByName('flip');Array.from(es).forEach(function (e){e.addEventListener('click', function(){document.getElementById('login-box').classList.toggle('flipped');});});document.getElementById('log').addEventListener('click', function(){document.getElementById('content').classList.add('blured');document.getElementById('loader-wrap').style.display='block';});");
 
-server.on('stream', (stream, headers) => {
+				res.end("</script></body></html>");
+				break;
+			}
+			
+			case '/main': {
+
+				break;
+			}
+
+			case '/login': {
+
+				break;
+			}
+	}		
+
+}
+
+
+/*server.on('stream', (stream, headers) => {
 
 
 	console.log(headers);
@@ -137,12 +189,12 @@ server.on('stream', (stream, headers) => {
 
 	// Carrega a sessão
 	GetSession(headers).then(session => {
-	
+
 		const method = headers[':method'];
 
 		const path = headers[':path'];
 		switch(path) {
-    		case '/': {
+			case '/': {
 				nonce = randomBytes(16).toString('hex');
 				//console.log(session);
 				// Envia o Header
@@ -166,13 +218,13 @@ server.on('stream', (stream, headers) => {
 					'x-permitted-cross-domain-policies': 'none',
 					'x-xss-protection': '1; mode=block',
 				},{ endStream : false}); 
-				// Header	
+				// Header
 				stream.write("<!DOCTYPE html><html itemscope itemtype='http://schema.org/WebSite'; lang="+session.lang+"><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta charset=utf-8><title itemprop=name>"+process.env.IndexTit+"</title><link rel=dns-prefetch href="+process.env.CDNBase+"><link rel=canonical href="+process.env.WWWBase+" itemprop=url><link rel=icon href='"+process.env.CDNBase+"img/logo.png' itemprop=image><link rel=preload href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/webfonts/fa-regular-400.woff2' as=font type='font/woff2' crossorigin=anonymous><link rel=preload href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/webfonts/fa-solid-900.woff2' as=font type='font/woff2' crossorigin=anonymous><meta name=description content='"+process.env.IndexDes+"' itemprop=description><meta name=keywords content='"+process.env.IndexKey+"'><meta name=apple-mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-status-bar-style content=black-translucent><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' rel=stylesheet integrity='sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9' crossorigin=anonymous><link href='"+process.env.CDNBase+"css/style.css' rel=stylesheet crossorigin=anonymous></head><body>");
 				// Block
 				stream.write("<div class=loader-wrap id=loader-wrap><div class=blocks><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div></div></div>");
 				// Scripts
 				stream.write("</body><script async src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm' crossorigin=anonymous></script><script nonce="+nonce+">const es=document.getElementsByName('flip');Array.from(es).forEach(function (e){e.addEventListener('click', function(){document.getElementById('login-box').classList.toggle('flipped');});});document.getElementById('log').addEventListener('click', function(){document.getElementById('content').classList.add('blured');document.getElementById('loader-wrap').style.display='block';});");
-				
+
 				stream.end("</script></body></html>");
 				break;
 			}
@@ -187,12 +239,12 @@ server.on('stream', (stream, headers) => {
 				break;
 			}
 
-    		case '/favicon.ico': {
+			case '/favicon.ico': {
 				stream.respond({
 					':status': '200',
 					'content-type': 'image/x-icon',
 				});
-				res.end();
+				stream.end();
 			}
 
 			default: {
@@ -203,13 +255,11 @@ server.on('stream', (stream, headers) => {
 				stream.end();
 			}
 		}
-
 	});
-
-});
+});*/
 
 /****************************************************************************************************/
-/* 	Mostra parâmetros e aguarda clientes															*/
+/* Mostra parâmetros e aguarda clientes																*/
 /****************************************************************************************************/
 const os = require('node:os');
 GetDate().then(dte => {
