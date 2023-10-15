7f86bc38960897a7e6425b7420e3f8013f054bae
Paulo <megamapa@gmail.com>
2023-10-14 22:01:51 -0300

    Small changes

--- index.js
+++ index.js
@@ -132,9 +132,14 @@ async function GetSession(headers) {
 /****************************************************************************************************/
 function onRequest(req, res) {
 	// Verifica se a conexão e HTTP/1 ou HTTP/2
-	const { socket: { alpnProtocol } } = req.httpVersion === '2.0' ? req.stream.session : req;
+	if (req.httpVersion === '2.0') {
+		const { socket: { alpnProtocol } } =  req.stream.session;
+		path = req.headers[':path'];
+	} else {
+		const { socket: { alpnProtocol } } =  req;
+		path = req.url;
+	}
 	// Responde
-	const path = req.httpVersion === '2.0' ? req.headers[':path'] : req.url;
 	switch(path) {
 			case '/': {
 				nonce = randomBytes(16).toString('hex');
@@ -178,86 +183,27 @@ function onRequest(req, res) {
 
 				break;
 			}
+
+			case '/favicon.ico': {
+				res.writeHead(200, {
+					'content-type': 'image/x-icon',
+				});
+				stream.end();
+				break;
+			}
+
+			default: {
+				res.writeHead(404, { 
+					'content-type': 'text/html; charset=UTF-8',
+				});
+				res.end(path+' - Not found');
+			}
 	}
 }
 
 
-/*server.on('stream', (stream, headers) => {
-
-
-	console.log(headers);
-
-
 	// Carrega a sessão
 	GetSession(headers).then(session => {
-
-		const method = headers[':method'];
-
-		const path = headers[':path'];
-		switch(path) {
-			case '/': {
-				nonce = randomBytes(16).toString('hex');
-				//console.log(session);
-				// Envia o Header
-				stream.respond({ //http2stream.respond https://http.dev/2/test
-					':status': '200',
-					'access-control-allow-methods': 'GET,POST',
-					'access-control-allow-origin': "'"+process.env.WWWBase+"'",
-					'cache-control': 'no-cache',
-					//'content-encoding': 'gzip',
-					'content-security-policy': "default-src 'self'; base-uri 'self'; script-src 'report-sample' 'nonce-"+nonce+"' cdn.jsdelivr.net/npm/ "+process.env.CDNBase+"; style-src 'self' 'report-sample' cdn.jsdelivr.net/npm/ "+process.env.CDNBase+"; object-src 'none'; frame-src 'self'; frame-ancestors 'none'; img-src 'self' "+process.env.CDNBase+"; font-src cdnjs.cloudflare.com/ajax/libs/font-awesome/; connect-src 'self' *.mapbox.com/; form-action 'self'; media-src 'self'; worker-src 'self'",
-					'content-type': 'text/html; charset=UTF-8',
-					'date': new Date().toUTCString(),
-					'permissions-policy': 'geolocation=(self "'+process.env.WWWBase+'")',
-					'referrer-policy': "no-referrer-when-downgrade",
-					'set-cookie': 'tk_v='+session.USID+'; Domain='+process.env.CKEBase+'; Path=/; Secure; HttpOnly', [http2.sensitiveHeaders]: ['set-cookie'],
-					'set-cookie': 'cross-site-cookie=name; SameSite=None; Secure; HttpOnly',
-					'strict-transport-security':'max-age=31536000; includeSubDomains; preload',
-					'vary': 'Accept-Encoding',
-					'x-content-type-options': 'nosniff',
-					'x-frame-options': 'DENY',
-					'x-permitted-cross-domain-policies': 'none',
-					'x-xss-protection': '1; mode=block',
-				},{ endStream : false}); 
-				// Header
-				stream.write("<!DOCTYPE html><html itemscope itemtype='http://schema.org/WebSite'; lang="+session.lang+"><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta charset=utf-8><title itemprop=name>"+process.env.IndexTit+"</title><link rel=dns-prefetch href="+process.env.CDNBase+"><link rel=canonical href="+process.env.WWWBase+" itemprop=url><link rel=icon href='"+process.env.CDNBase+"img/logo.png' itemprop=image><link rel=preload href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/webfonts/fa-regular-400.woff2' as=font type='font/woff2' crossorigin=anonymous><link rel=preload href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/webfonts/fa-solid-900.woff2' as=font type='font/woff2' crossorigin=anonymous><meta name=description content='"+process.env.IndexDes+"' itemprop=description><meta name=keywords content='"+process.env.IndexKey+"'><meta name=apple-mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-status-bar-style content=black-translucent><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' rel=stylesheet integrity='sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9' crossorigin=anonymous><link href='"+process.env.CDNBase+"css/style.css' rel=stylesheet crossorigin=anonymous></head><body>");
-				// Block
-				stream.write("<div class=loader-wrap id=loader-wrap><div class=blocks><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div><div class=block></div></div></div>");
-				// Scripts
-				stream.write("</body><script async src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm' crossorigin=anonymous></script><script nonce="+nonce+">const es=document.getElementsByName('flip');Array.from(es).forEach(function (e){e.addEventListener('click', function(){document.getElementById('login-box').classList.toggle('flipped');});});document.getElementById('log').addEventListener('click', function(){document.getElementById('content').classList.add('blured');document.getElementById('loader-wrap').style.display='block';});");
-
-				stream.end("</script></body></html>");
-				break;
-			}
-
-			case '/main': {
-
-				break;
-			}
-
-			case '/login': {
-
-				break;
-			}
-
-			case '/favicon.ico': {
-				stream.respond({
-					':status': '200',
-					'content-type': 'image/x-icon',
-				});
-				stream.end();
-			}
-
-			default: {
-				stream.respond({
-					':status': '404',
-					'content-type': 'text/html; charset=UTF-8',
-				});
-				stream.end();
-			}
-		}
-	});
-});*/
 
 /****************************************************************************************************/
 /* Mostra parâmetros e aguarda clientes																*/
