371a1f24809b9a92e06a4eea986cdcafd4edd845
Paulo <megamapa@gmail.com>
2023-10-15 22:15:39 -0300

    Small changes

---
+++ lang/pt-BR/index.js
@@ -0,0 +1,20 @@
+// constants.js
+
+'use strict';
+
+let constants = {
+    _TITLE          : 'Login',
+    _DESCRIPTION    : 'Gerenciamento de rastreadores e chips M2M',
+    _KEYWORDS       : 'rastreador m2m precissão tracker localizador IOT',
+
+    _LOGIN          : 'LOGIN',
+    _NAME           : 'Nome / E-mail',
+    _PASS           : 'Senha',
+    _FORGOT         : 'Esqueci minha senha',
+    _STAY           : 'Ficar conectado',
+    _SEND           : 'ENTRAR',
+    _CLOSE          : 'FECHAR',
+
+};
+
+module.exports = Object.freeze(constants);
--- lang/zh-CN/login.php
+++
@@ -1,3 +0,0 @@
-<?php
- define('_LOGIN', 'Tentativa de login.<br>IP: %1$s<br>Navegador: %2$s');
-?>
